3d92e99ca30916fd8303a8024d36f6a1b7a19add
Bapu Shinde <shindebapu97@gmail.com>
2024-12-29 13:40:29 +0530

    intial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Student {
+
+public static void main(String args[])
+{
+	
+System.out.println("hi, this code is inside student class of dir repo");
+}
+
+
+	
+	
+}
