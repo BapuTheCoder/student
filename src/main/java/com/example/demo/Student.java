79b9460c78b4195f84473f8382ed30da6bf4a434
Bapu Shinde <shindebapu97@gmail.com>
2024-12-29 16:05:16 +0530

    upadted finaally

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 package com.example.demo;
 
 public class Student {
 
-public static void main(String args[])
-{
-	
-System.out.println("hi, this code is inside student class of dir repo");
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Age: " + age);
+    }
 }
 
-
-	
-	
-}
